dd7687b126fa12f38d78bc3d524baf9325edf628
xiaoguo <348383021@qq.com>
2022-07-15 16:12:54 +0800

    完成了login页面

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getToken, setToken } from '@/utils'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(localStorage.getItem('HEIMA_TOUTIAO_TOKEN')) || {}
+    user: getToken() || {}
   },
   getters: {},
   mutations: {
     setUser (state, payload) {
       state.user = payload
-      localStorage.setItem('HEIMA_TOUTIAO_TOKEN', JSON.stringify(payload))
+      setToken(payload)
     }
   },
   actions: {},
---
+++ src/utils/auth.js
@@ -0,0 +1,5 @@
+import storage from './storage'
+const TOKEN_KEY = 'HEIMA_TOUTIAO_TOKEN'
+export const getToken = () => storage.get(TOKEN_KEY)
+export const setToken = (token) => storage.set(TOKEN_KEY, token)
+export const removeToken = () => storage.remove(TOKEN_KEY)
